eabafd200c8fae2ecd8af75a5ba5cd5b96b8a56b
PyRchitect <marin.pirsic@gmail.com>
2024-01-24 13:59:46 +0100

    MINESWEEPER

--- M02/MINESWEEPER_v5.py
+++ M02/MINESWEEPER_v5.py
@@ -1,9 +1,188 @@
 import os
+import math
 import random as rn
 import itertools as it
 import urwid as uw
 
-from interface import Interface
+import tree_node as tn
+
+class Interface():
+	config = {
+		"d_sep": '- ',
+		"m_sep": 20}
+
+	separator = config['d_sep'] * config['m_sep']
+
+	def cls_check():
+		return 'cls' if os.name =='nt' else 'clear'
+
+	class SimpleMessage():
+		def __init__(self,
+			   text='',
+			   sep=' ',
+			   end='\n'):
+
+			self.text = text
+			self.sep = sep
+			self.end = end			
+
+		def show(self):
+			print(self.text,sep=self.sep,end=self.end)
+
+	class MultiMessage():
+		def __init__(self,
+			   lines='',
+			   seps=' ',
+			   ends='\n',
+			   length=0):
+			
+			length = length or len(lines)
+						
+			def init_list(L,length,default):
+				return [default]*length if L==default else L
+			
+			lines = init_list(lines,length,'')
+			seps = init_list(seps,length,' ')
+			ends = init_list(ends,length,'\n')
+
+			self.messages = []
+			for (l,s,e) in zip(lines,seps,ends):
+				self.messages.append(Interface.SimpleMessage(l,s,e))
+		
+		def show(self):
+			for m in self.messages:
+				m.show()
+
+	class SimpleGetter():
+		def __init__(self,
+			   choice_label='',
+			   choice_sep=' ',
+			   choice_type=None,
+			   choice_test=None,
+			   choice_error=None):
+			
+			self.choice_label = choice_label
+			self.choice_sep = choice_sep
+			self.choice_type = choice_type
+			self.choice_test = choice_test
+			self.choice_error = choice_error
+		
+		def get_input(self):
+			i_test = False
+			while i_test == False:
+				try:
+					Interface.SimpleMessage(self.choice_label,end=' ').show()
+					choice = input().split(self.choice_sep)
+
+					if not self.choice_test:
+						self.choice_test = [lambda x:x for _ in len(choice)]
+
+					for i,c in enumerate(choice):
+						c = self.choice_type[i](c)
+						assert self.choice_test[i](c)						
+						choice[i] = c
+				except:
+					Interface.SimpleMessage(self.choice_error).show()
+					i_test == False
+				else:
+					i_test = True
+				finally:
+					Interface.SimpleMessage(Interface.separator)
+			return choice
+
+	class MultiGetter():
+		def __init__(self,
+			   choice_labels='',
+			   choice_seps=' ',
+			   choice_types=None,
+			   choice_tests=None,
+			   choice_errors=None,
+			   length=0):
+			
+			length = length or len(choice_labels)
+			
+			def init_list(L,length,default):
+				return [default]*length if L==default else L
+
+			choice_labels = init_list(choice_labels,length,'')
+			choice_seps = init_list(choice_seps,length,'')
+			choice_types = init_list(choice_types,length,None)
+			choice_tests = init_list(choice_tests,length,None)
+			choice_errors = init_list(choice_errors,length,None)
+
+			self.getters = []
+			for (cl,cs,ct,ctest,ce) in zip(
+				choice_labels,
+				choice_seps,
+				choice_types,
+				choice_tests,
+				choice_errors):
+				self.getters.append(Interface.SimpleGetter(cl,cs,ct,ctest,ce))
+		
+		def get_inputs(self):
+			choices = []
+			for g in self.getters:
+				choices.append(g.get_input())
+			return choices
+			
+	class SimpleMenu():
+		def __init__(self,
+			   title='',
+			   options='',
+			   choice_label='',
+			   choice_sep=' ',
+			   choice_type=None,
+			   choice_test=None,
+			   choice_error=None):
+
+			self.title = title
+
+			self.options = options
+
+			self.getter = Interface.SimpleGetter(
+				choice_label,
+				choice_sep,
+				choice_type,
+				choice_test,
+				choice_error)
+		
+		def show_text(self):
+			Interface.SimpleMessage('\n'+self.title).show()
+			Interface.SimpleMessage(Interface.separator).show()
+
+			for option in self.options:
+				Interface.SimpleMessage(option).show()
+
+			Interface.SimpleMessage(Interface.separator).show()
+		
+		def show_input(self):
+			return Interface.SimpleGetter.get_input(self.getter)
+		
+		def show(self):
+			os.system(Interface.cls_check())
+			self.show_text()
+			return self.show_input()
+
+	class CascadingMenu(tn.TreeNode):
+		def __init__(self,data=None):
+			# set basic menu options (palette,etc.)
+			tn.TreeNode.__init__(self,data)
+		
+		def show_parents(self):
+			for parent in self.parents():
+				parent.data.show_text()
+		
+		def show_text(self):
+			self.data.show_text()
+		
+		def show_input(self):
+			return self.data.show_input()
+		
+		def show(self):
+			os.system(Interface.cls_check())
+			self.show_parents()
+			self.show_text()
+			return self.show_input()
 
 class Board():
 	config = {
@@ -556,11 +735,7 @@ class Graphics():
 			...
 
 		def translate_move(x,y):
-			test = True
-			if test:
-				return (True,x,y)
-			else:
-				return (False,x,y)
+			...
 
 	class MouseMode():
 
@@ -587,6 +762,10 @@ class Graphics():
 				(self.values['cross'],'default','default'),
 				(self.values['corner'],'default','default'),
 				("ENDC",'default','default')]
+			
+			# to be populated with board data
+			self.offset = (0,0)
+			self.jump_size = (1,1)
 
 		def render(self,board:'Board',board_type):
 			if board_type not in ['real','active']:
@@ -747,57 +926,59 @@ class Graphics():
 		def __exit__(self, exc_type, exc_value, exc_traceback):
 			...
 
-		def translate_move(self,r,c,board:'Board'):
+		def set_offset(self,board:'Board'):
 			gc = self.config
 
 			def add_h_sep_rows():
 				return gc['v_spacing']+gc['h_separate']+gc['v_spacing']
 			def add_v_sep_cols():
 				return gc['h_spacing']+gc['v_separate']+gc['h_spacing']
+			
+			# SET JUMP SIZE:
+			self.jump_size = (add_v_sep_cols()+1,add_v_sep_cols()+1)
 
 			# SET VERTICAL OFFSET:
-			v_start = 0						# empty row
+			r0 = 0						# empty row
 
 			# TEST - NEED TO SKIP IF REAL BOARD REVEALED:
-			v_start += 1					# board title
-			v_start += 1					# title rows
-			v_start+=add_h_sep_rows()		# separator
+			r0 += 1						# board title
+			r0 += 1						# title rows
+			r0+=add_h_sep_rows()		# separator
 			for _ in range(board.height):
-				v_start += 1				# value row
-				v_start+=add_h_sep_rows()
-			v_start += 1					# title rows
-			v_start += 1					# caption
+				r0 += 1					# value row
+				r0+=add_h_sep_rows()
+			r0 += 1						# title rows
+			r0 += 1						# caption
 
-			v_start += 1					# empty row
+			r0 += 1						# empty row
 
 			# ACTIVE BOARD:
-			v_start += 1					# board title
-			v_start += 1					# title rows
-			v_start+=add_h_sep_rows()		# separator
-			v_start += 1					# value row
+			r0 += 1						# board title
+			r0 += 1						# title rows
+			r0+=add_h_sep_rows()		# separator
+			r0 += 1						# value row
 
 			# SET HORIZONTAL OFFSET:
-			h_start = 0						# empty col
+			c0 = 0						# empty col
 
-			h_start += gc['v_separate']		# separator
-			h_start += gc['h_spacing']		# spacing
-			h_start += gc['axis_name_width']# row title
-			h_start += gc['h_spacing']+gc['value_width']-gc['axis_name_width']
-			h_start += gc['v_separate']		# separator
-			h_start += gc['h_spacing']		# spacing
-			h_start += 1					# value
+			c0 += gc['v_separate']		# separator
+			c0 += gc['h_spacing']		# spacing
+			c0 += gc['axis_name_width']# row title
+			c0 += gc['h_spacing']+gc['value_width']-gc['axis_name_width']
+			c0 += gc['v_separate']		# separator
+			c0 += gc['h_spacing']		# spacing
+			c0 += 1						# value
 
-			r0 = r - v_start
-			c0 = c - h_start
-			# # # TEST
-			return (True,r0,c0)
-			# # # TEST
+			self.offset = (r0,c0)
+			
+		def translate_move(self,r,c):
+			(rt,rrem) = math.modf(r - self.offset[0])/self.jump_size[0]
+			(ct,crem) = math.modf(c - self.offset[1])/self.jump_size[1]
 
-			test = True
-			if test:
-				return (True,r,c)
+			if not (rrem and crem):
+				return (True,rt,ct)
 			else:
-				return (False,r,c)
+				return (False,False,False)
 
 	def set_renderer(self,input_mode):
 		self.renderer = self.renderers[input_mode](self.config,self.values)
@@ -915,7 +1096,7 @@ def play(ms:'Game'):
 
 		elif ms.graphics.renderer.__class__ == ms.graphics.renderers[1]:
 			with ms.graphics.renderer as gr:
-
+				gr.set_offset(ms.board)
 				(r,c,t) = (False,False,False)
 
 				def exit_to_menu(*args):
@@ -924,32 +1105,16 @@ def play(ms:'Game'):
 					raise uw.ExitMainLoop()
 				
 				def translate(render,r_mouse,c_mouse,t_mouse):
-
-					# SHOW INFO AS A DIALOG BOX: "... has no attribute 'rows : FLOW > BOX?"
-					# nonlocal fill
-					# txt = uw.Text(f'r: {r_mouse} | c: {c_mouse} | t: {t_mouse}',align='center')
-					# done = uw.Button('Ok')
-					# uw.connect_signal(done,'click',exit_to_menu)
-					# done_map = uw.AttrMap(uw.Button('Ok'),None,focus_map='reversed')
-					# pile = uw.Pile([txt,done_map])
-					# filler = uw.Filler(pile,'top',height=('relative',30),min_height=3)
-					# padding = uw.Padding(filler,left=2,right=2)
-					# overlay = uw.Overlay(
-					# 	padding,uw.SolidFill('\N{MEDIUM SHADE}'),
-					# 	align='center',width=('relative',60),
-					# 	valign='middle',height=('relative',60),
-					# 	min_width=20,min_height=9)
-					# fill.original_widget = overlay
-					
 					nonlocal r,c,t
 
-					(test,r0,c0) = ms.graphics.renderer.translate_move(r_mouse,c_mouse,ms.board)
-					# TEST: SIMPLE PRINT
-					print(f'r: {r_mouse} | c: {c_mouse} | t: {t_mouse} || r0: {r0} | c0: {c0}')
+					(test,rt,ct) = gr.translate_move(r_mouse,c_mouse)
+					# # # TEST
+					# print(f'r: {r_mouse} | c: {c_mouse} | t: {t_mouse} || r0: {r0} | c0: {c0}')
+					# # # TEST
 
-					# if test:
-					# 	(r,c,t) = (r_mouse,c_mouse,t_mouse)
-					# 	exit_to_menu()
+					if test:
+						(r,c,t) = (rt,ct,t_mouse)
+						exit_to_menu()
 
 				render = gr.display(ms)
 				uw.set_encoding("UTF-8")
