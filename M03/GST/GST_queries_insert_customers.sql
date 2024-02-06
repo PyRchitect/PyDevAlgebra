958475e14300622eb04cfd551f35c8274636ee19
PyRchitect <marin.pirsic@gmail.com>
2024-02-06 22:42:22 +0100

    GST

---
+++ M03/GST/GST_queries_insert_customers.sql
@@ -0,0 +1,48 @@
+USE GST
+GO
+
+INSERT INTO CUSTOMERS(customerName,customerAddress,customerPostalCode,customerCity,customerOIB,customerWebsite) VALUES
+('Dubravkin put','Dubravkin put 2','10000','Zagreb','34414865362','https://www.dubravkin-put.com/'),
+('Vinodol','Teslina 10','10000','Zagreb','75508711169','https://vinodol-zg.hr/');
+
+/*
+Takenoko
+https://www.takenoko.hr/
+Masarykova 22, 10000 Zagreb, Hrvatska
+OIB: 79733218169
+
+ManO2
+https://mano2.hr/
+Radnička cesta 50
+OIB: 86637426089
+
+Carpaccio
+https://ristorantecarpaccio.hr/
+Teslina 14 10000 Zagreb
+OIB: 83706912258
+
+Rougemarin
+https://www.rougemarin.hr/
+Frane Folnegovića 10, 10000 Zagreb
+OIB: 20822976890
+
+Lari i Penati
+http://www.laripenati.hr/
+Petrinjska 42a 10000 Zagreb
+OIB: 88529228879
+
+Boban
+http://www.boban.hr/
+Gajeva 9, 10000 Zagreb
+OIB: 53656152979
+
+BOTA-ŠARE d.o.o.
+https://www.bota-sare.hr/
+Ulica kralja Zvonimira 124, 10000 Zagreb
+OIB: 74950041813
+
+IZAKAYA
+https://www.izakaya.hr/
+Selska 90b, 10000 Zagreb
+OIB: 07919388411
+*/
--- M03/GST/GST_queries_table_creation.sql
+++ M03/GST/GST_queries_table_creation.sql
@@ -2,15 +2,25 @@ CREATE DATABASE GST;
 GO;
 
 USE GST;
-GO;
+GO
 
+DROP TABLE [dbo].[CUSTOMERS]
 CREATE TABLE [dbo].[CUSTOMERS] (
-    [customerId] INT           IDENTITY (1, 1) NOT NULL,
-    [firstName]  VARCHAR (MAX) NULL,
-    [lastName]   VARCHAR (MAX) NULL,
-    PRIMARY KEY CLUSTERED ([customerId] ASC)
+    [customerId]         INT           IDENTITY (1, 1) NOT NULL,
+    [customerName]       VARCHAR (100) NOT NULL,
+    [customerAddress]    VARCHAR (100) NOT NULL,
+    [customerPostalCode] VARCHAR (5)   NOT NULL,
+    [customerCity]       VARCHAR (50)  NOT NULL,
+    [customerOIB]        CHAR (11)     NOT NULL,
+    [customerWebsite]    VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([customerId] ASC),
+    CONSTRAINT [digitCustomerOIB] CHECK (NOT [customerOIB] like '%[^0-9]%'),
+    CONSTRAINT [digitCustomerPC] CHECK (NOT [customerPostalCode] like '%[^0-9]%'),
+    CONSTRAINT [lenCustomerOIB] CHECK (datalength([customerOIB])=(11)),
+    CONSTRAINT [lenCustomerPC] CHECK (datalength([customerPostalCode])=(5))
 );
 
+DROP TABLE [dbo].[FTL]
 CREATE TABLE [dbo].[FTL] (
     [FSMA FTL]                    NVARCHAR (255) NOT NULL,
     [PLU]                         INT            NOT NULL,
@@ -32,18 +42,7 @@ CREATE TABLE [dbo].[FTL] (
     CONSTRAINT [PK_FTL] PRIMARY KEY CLUSTERED ([PLU] ASC)
 );
 
-CREATE TABLE [dbo].[INVENTORY] (
-    [inventoryId] INT            NOT NULL,
-    [itemNum]     INT            NOT NULL,
-    [vendorId]    INT            NOT NULL,
-    [price]       DECIMAL (4, 2) NOT NULL,
-    [qtyInStock]  INT            NOT NULL,
-    [qtyReStock]  INT            NOT NULL,
-    CONSTRAINT [PK_INVENTORY] PRIMARY KEY CLUSTERED ([inventoryId] ASC),
-    CONSTRAINT [FK_INVENTORY_FTL] FOREIGN KEY ([itemNum]) REFERENCES [dbo].[PRODUCTS] ([itemNum]),
-    CONSTRAINT [FK_INVENTORY_VENDORS] FOREIGN KEY ([vendorId]) REFERENCES [dbo].[VENDORS] ([vendorId])
-);
-
+DROP TABLE [dbo].[NONFTL]
 CREATE TABLE [dbo].[NONFTL] (
     [FSMA FTL]                    NVARCHAR (255) NOT NULL,
     [PLU]                         INT            NOT NULL,
@@ -65,16 +64,21 @@ CREATE TABLE [dbo].[NONFTL] (
     CONSTRAINT [PK_NONFTL] PRIMARY KEY CLUSTERED ([PLU] ASC)
 );
 
+DROP TABLE [dbo].[PRODUCTS]
 CREATE TABLE [dbo].[PRODUCTS] (
-    [itemNum] INT NOT NULL,
-    [PLU]     INT NOT NULL,
-    CONSTRAINT [PK_PRODUCTS] PRIMARY KEY CLUSTERED ([itemNum] ASC),
+    [productId]       INT            IDENTITY (1, 1) NOT NULL,
+    [PLU]             INT            NOT NULL,
+    [productVendorId] INT            NOT NULL,
+    [price]           DECIMAL (4, 2) NOT NULL,
+    CONSTRAINT [PK_PRODUCTS] PRIMARY KEY CLUSTERED ([productId] ASC),
     CONSTRAINT [digitPLU] CHECK (NOT [PLU] like '%[^0-9]%'),
     CONSTRAINT [lenPLU] CHECK (datalength([PLU])=(5)),
     CONSTRAINT [FK_PRODUCTS_FTL] FOREIGN KEY ([PLU]) REFERENCES [dbo].[FTL] ([PLU]),
-    CONSTRAINT [FK_PRODUCTS_NONFTL] FOREIGN KEY ([PLU]) REFERENCES [dbo].[NONFTL] ([PLU])
+    CONSTRAINT [FK_PRODUCTS_NONFTL] FOREIGN KEY ([PLU]) REFERENCES [dbo].[NONFTL] ([PLU]),
+    CONSTRAINT [FK_PRODUCTS_VENDORS] FOREIGN KEY ([productVendorId]) REFERENCES [dbo].[VENDORS] ([vendorId])
 );
 
+DROP TABLE [dbo].[VENDORS]
 CREATE TABLE [dbo].[VENDORS] (
     [vendorId]         INT           IDENTITY (1, 1) NOT NULL,
     [vendorName]       VARCHAR (100) NOT NULL,
@@ -82,10 +86,22 @@ CREATE TABLE [dbo].[VENDORS] (
     [vendorPostalCode] VARCHAR (5)   NOT NULL,
     [vendorCity]       VARCHAR (50)  NOT NULL,
     [vendorOIB]        CHAR (11)     NOT NULL,
-    [vendorWebsite]    VARCHAR (100) NULL,
+    [vendorWebsite]    VARCHAR (100) NOT NULL,
     CONSTRAINT [PK_VENDORS] PRIMARY KEY CLUSTERED ([vendorId] ASC),
-    CONSTRAINT [digitOIB] CHECK (NOT [vendorOIB] like '%[^0-9]%'),
-    CONSTRAINT [digitPC] CHECK (NOT [vendorPostalCode] like '%[^0-9]%'),
-    CONSTRAINT [lenOIB] CHECK (datalength([vendorOIB])=(11)),
-    CONSTRAINT [lenPC] CHECK (datalength([vendorPostalCode])=(5))
+    CONSTRAINT [digitVendorOIB] CHECK (NOT [vendorOIB] like '%[^0-9]%'),
+    CONSTRAINT [digitVendorPC] CHECK (NOT [vendorPostalCode] like '%[^0-9]%'),
+    CONSTRAINT [lenVendorOIB] CHECK (datalength([vendorOIB])=(11)),
+    CONSTRAINT [lenVendorPC] CHECK (datalength([vendorPostalCode])=(5))
+);
+
+DROP TABLE [dbo].[INVENTORY]
+CREATE TABLE [dbo].[INVENTORY] (
+    [inventoryId] INT NOT NULL,
+    [productId]   INT NOT NULL,
+    [vendorId]    INT NOT NULL,
+    [qtyInStock]  INT NOT NULL,
+    [qtyReStock]  INT NOT NULL,
+    CONSTRAINT [PK_INVENTORY] PRIMARY KEY CLUSTERED ([inventoryId] ASC),
+    CONSTRAINT [FK_INVENTORY_FTL] FOREIGN KEY ([productId]) REFERENCES [dbo].[PRODUCTS] ([productId]),
+    CONSTRAINT [FK_INVENTORY_VENDORS] FOREIGN KEY ([vendorId]) REFERENCES [dbo].[VENDORS] ([vendorId])
 );
